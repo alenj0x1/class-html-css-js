3c7a7e603dff110b6785b72741dc487903bb1943
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-05-30 17:52:06 -0500

    feat: included tailwindcss and editor setup complete

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
-const KEY_TITLE_PAGE = "title_page";
+const KEY_EDITABLE_JSON = "editableJSON";
+const KEY_EDITABLE_ATTRIBUTE = "data-editable";
 const headerTitle = document.getElementById("header-title");
 const modeEditable = document.getElementById("mode-editable");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("se cargo todo");
-  headerTitle.textContent =
-    localStorage.getItem(KEY_TITLE_PAGE) ?? "Mi portafolio";
+  initEditableJSON();
 });
 
-function changeTitlePage(e) {
-  document.querySelector("body").classList.add("body-black");
+function initEditableJSON() {
+  const elements = document.querySelectorAll(`[${KEY_EDITABLE_ATTRIBUTE}]`);
 
-  const newTitle = e.target.textContent;
-  localStorage.setItem(KEY_TITLE_PAGE, newTitle);
+  const findEditableJSON = localStorage.getItem(KEY_EDITABLE_JSON);
+  if (!findEditableJSON) {
+    createEditableJSON(elements);
+  }
+
+  elements.forEach((el) => {
+    // Establecer valores almacenados
+    const editableJSON = localStorage.getItem(KEY_EDITABLE_JSON);
+    const editableJSONParsed = JSON.parse(editableJSON);
+
+    const key = el.getAttribute(KEY_EDITABLE_ATTRIBUTE);
+    el.textContent = editableJSONParsed[key];
+
+    // Editando
+    el.addEventListener("input", (e) => {
+      const editableJSON = localStorage.getItem(KEY_EDITABLE_JSON);
+      const editableJSONParsed = JSON.parse(editableJSON);
+
+      const key = e.target.getAttribute(KEY_EDITABLE_ATTRIBUTE);
+      editableJSONParsed[key] = e.target.textContent;
+
+      localStorage.setItem(
+        KEY_EDITABLE_JSON,
+        JSON.stringify(editableJSONParsed)
+      );
+    });
+
+    el.addEventListener("focus", (_) => {
+      onFocusEditable();
+    });
+
+    el.addEventListener("blur", (_) => {
+      onBlurEditable();
+    });
+  });
+}
+
+function createEditableJSON(elements) {
+  const newJSON = {};
+
+  elements.forEach((el) => {
+    const key = el.getAttribute(KEY_EDITABLE_ATTRIBUTE);
+    newJSON[key] = el.textContent;
+  });
+
+  localStorage.setItem(KEY_EDITABLE_JSON, JSON.stringify(newJSON));
 }
 
 function onFocusEditable() {
